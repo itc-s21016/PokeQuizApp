3fad8a02cc94194b28ba215e463d91ed0341b9fb
s21016 <s21016@std.it-college.ac.jp>
2023-01-05 02:37:55 +0900

    結果が表示されるようにした。

--- app/src/main/java/jp/ac/it_college/std/s21016/pokemonquiz/answerScreen.kt
+++ app/src/main/java/jp/ac/it_college/std/s21016/pokemonquiz/answerScreen.kt
@@ -5,55 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.navArgs
+import jp.ac.it_college.std.s21016.pokemonquiz.databinding.FragmentAnswerScreenBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [answerScreen.newInstance] factory method to
- * create an instance of this fragment.
- */
-class answerScreen : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class Answer : Fragment() {
+    private var _binding: FragmentAnswerScreenBinding? = null
+    private val binding get() = _binding!!
+    val args:AnswerArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_answer_screen, container, false)
-    }
+        _binding = FragmentAnswerScreenBinding.inflate(inflater, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment answerScreen.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            answerScreen().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        binding.collectText.apply {
+            text = "${args.collecto}/10 問正解！！"
+        }
+        binding.btGenSelect.setOnClickListener {
+            Navigation.findNavController(it).navigate(
+                AnswerDirections.actionAnswerScreenToGenSelectScreen()
+            )
+        }
+        return binding.root
     }
 }
